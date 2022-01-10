cfa8238fbf5a83af95287b5e61bf2a874f71a012
Andrew Stowell <andrew@digitaladapt.com>
2022-01-10 19:19:20 +0000

    elasticache now working

--- handler.mjs
+++ handler.mjs
@@ -1,7 +1,28 @@
 'use strict';
 
-import RedisCache from './RedisCache.mjs';
-import fetch from 'node-fetch'
+import { createClient } from 'redis';
+import fetch from 'node-fetch';
+
+// return a redis client
+const getRedis = async () => {
+    // get config from environment
+    const [redisHost, redisPort] = process.env.REDIS_ENDPOINT.split(':');
+
+    const client = createClient({
+        socket: {
+            host: redisHost,
+            port: redisPort
+        }
+    });
+
+    client.on('error', (error) => {
+        console.log('Redis Client Error:', error);
+    });
+
+    await client.connect();
+
+    return client;
+};
 
 const token = (asset) => {
     // convert "BNB.BUSD-BD1" into "BUSD"
@@ -9,34 +30,37 @@ const token = (asset) => {
 };
 
 const midgard = async (wallets, pagination) => {
+    // TODO
     // const url = "https://midgard/{wallets}?offset={pagination}
     // fetch(url).then
 };
 
 export const hello = async (event) => {
-  const CACHE_KEY = 'CACHE_KEY'
-  let res = {}
-  let checkCache = await RedisCache.get(CACHE_KEY)
-  if (checkCache) {
-    res = checkCache
-  } else {
-    await RedisCache.set(CACHE_KEY, {'message': 'Hello World!'})
-    res = {'message': 'Set cache success!'}
-  }
+    let res = {}
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-		res: res,
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+    const redis = await getRedis();
+    if (await redis.exists('CACHE_KEY')) {
+        // exists
+        res = await redis.get('CACHE_KEY');
+    } else {
+        // does NOT exist
+        res = await redis.set('CACHE_KEY', JSON.stringify({'message': 'Hello World!'}));
+    }
+    await redis.quit();
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+    return {
+        statusCode: 200,
+        body: JSON.stringify(
+            {
+                message: 'Go Serverless v1.0! Your function executed successfully!',
+                res: res,
+                input: event,
+            },
+            null,
+            2
+        ),
+    };
+
+    // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+    // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };

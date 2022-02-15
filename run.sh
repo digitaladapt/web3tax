9a2040a5e61d75b6533b7879b1d97bd597090a20
Andrew Stowell <andrew@digitaladapt.com>
2022-02-15 16:02:24 +0000

    one of the midgards is down, switching to the other.

--- run.sh
+++ run.sh
@@ -6,8 +6,8 @@
 # midgard pages results (max 50)
 # we have to point to a Midgard instance (data provider)
 # possible values:
-# Official Midgard (works just fine)    # https://midgard.thorchain.info/v2/actions?limit=50&address={WALLETS}&offset={OFFSET}
-# NineRealms Midgard (gave some issues) # https://midgard.ninerealms.com/v2/actions?limit=50&address={WALLETS}&offset={OFFSET}
+# Official Midgard   # https://midgard.thorchain.info/v2/actions?limit=50&address={WALLETS}&offset={OFFSET}
+# NineRealms Midgard # https://midgard.ninerealms.com/v2/actions?limit=50&address={WALLETS}&offset={OFFSET}
 # port: tcp port to bind, for web interface and api calls (in production, you'd want a proxy like nginx to handle https)
 # ttl: time-to-live (in seconds); production 7200 (2 hours), test 120 (2 minutes)
 # non-test version is wrapped in loop-forever, so that it restarts if it crashes.
@@ -16,7 +16,7 @@ while true; do \
 REDIS_ENDPOINT=localhost:6379 \
 REDIS_PREFIX='live_' \
 MIDGARD_LIMIT=50 \
-MIDGARD_URL='https://midgard.thorchain.info/v2/actions?limit=50&address={WALLETS}&offset={OFFSET}' \
+MIDGARD_URL='https://midgard.ninerealms.com/v2/actions?limit=50&address={WALLETS}&offset={OFFSET}' \
 PORT=3000 \
 TTL=7200 \
 node server.mjs 2>&1 | tee -a output.log; \
